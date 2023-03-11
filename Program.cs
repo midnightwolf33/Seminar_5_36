dc0dfbf54d626b20742c9c2213089c44b740e374
midnight33 <gra33@bk.ru>
2023-03-11 13:48:25 +0300

    Задача 36

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] Gen1DArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int SumPos (int [] arr)
+{
+    int res = 0;
+    for (int i = 1; i < arr.Length; i = i + 2)
+    {
+        res += arr[i];
+    }
+    return res;
+}
+
+int[] testArr = Gen1DArray(10,1,10);
+
+Print1DArr(testArr);
+int oddValueSum = SumPos(testArr);
+PrintData("Сумма нечетных элементов массива равна: " + oddValueSum);
